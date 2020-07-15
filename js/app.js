42022a81c992e8ca301c76674008afdbe703698e
Alan Karlo Velasco Mangubat <karlomangubat92@gmail.com>
2020-07-15 14:38:21 +0900

    added loops

--- js/app.js
+++ js/app.js
@@ -23,19 +23,65 @@ function addUserName() {
 
 function addPreference(){
     var preference = prompt('Do you like prequals or sequals?');
-var response;
+    var response;
 
-if (preference === 'Prequals') {
-    response = 'Prequals are Great!';
-} else if (preference === 'sequals') {
-    response = 'Sequals are Fabulous!';
-} else {
-    response = "Fine!";
+while (preference !== 'prequals' && preference !== 'sequals'){
+    preference = prompt('Please answer exactly! Do you like prequals or sequals?')
 }
+
+if (preference === 'prequals') {
+    item = '<p><img src="https://i.pinimg.com/originals/22/4f/9d/224f9d64ad01d1bf5937a46ffe99eaec.jpg"></p>';
+} else if (preference === 'sequals') {
+    item = '<img src="https://live.staticflickr.com/65535/49110577696_040d037ae0.jpg"></p>';
+} 
+
+// need to add the images instead of the response
 
 return document.write('<h3>' + response + '</h3>')
 }
 
+//helper function
+function getCount() {
+    var count = prompt('How many star wars movies have you watched?');
+// ( false || false)
+    while (isNan(count) || count === '') {
+        count = prompt('please enter a number! How many star wars movies have you watched?');
+    }
+
+    return document.write(count);
+}
+
+ function getStarCount(){
+    var starCount = prompt('How many stars would you rate the experience? 1 to 5. ')
+    
+    while(isNan(count) || count === ''){
+ count = prompt('please enter a number! How would you rate that experience?');
+}
+return starCount
+}
+
+function showOrder(){
+    var result = '';
+    var itemType = addPreference();
+    var total = getCount();
+
+    for (var i = 0; i < total; i++){
+        result = result + '<p>' + itemType + '</p>';
+    }
+
+    return document.write(result);
+}
+
+function showRating(){
+    var result = '';
+    var starCount = getStarCount();
+  
+    for (var i = 0; i < starCount; i++){
+      result = result + '<p><img src="images/star.png"></p>';
+    }
+  
+    return document.write(result);
+  }
 //I want a white cake with lavender frosting and lemon filling.
 function buildCake(cakeType, frostingType, fillingType){
     var message = 'I want a ' + cakeType + 'cake with ' + frostingType + ' frosting and' + fillingType + ' filling.';
